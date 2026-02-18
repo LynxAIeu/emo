c4a4fe4520943462f7cf91c75588d7313ff4c348
Oliver <ogh@lynxai.team>
2026-02-18 20:50:29 +0100

    clean: rename flags variable

--- codegen/main.go
+++ codegen/main.go
@@ -16,43 +16,43 @@ import (
 func main() {
 	fmt.Println("[codegen] Generator of emo source code")
 
-	dartf := flag.Bool("dart", false, "generate Dart code")
-	docf := flag.Bool("doc", false, "generate the documentation")
-	gof := flag.Bool("go", false, "generate Go code")
-	pyf := flag.Bool("py", false, "generate Python code")
-	tsf := flag.Bool("ts", false, "generate Typescript code")
+	dartFlag := flag.Bool("dart", false, "generate Dart code")
+	docFlag := flag.Bool("doc", false, "generate the documentation")
+	goFlag := flag.Bool("go", false, "generate Go code")
+	pyFlag := flag.Bool("py", false, "generate Python code")
+	tsFlag := flag.Bool("ts", false, "generate Typescript code")
 	flag.Parse()
 
-	hasFlag := *dartf || *docf || *gof || *pyf || *tsf
-	enableAll := !hasFlag
-	if enableAll {
+	hasFlag := *dartFlag || *docFlag || *goFlag || *pyFlag || *tsFlag
+	if !hasFlag {
+		*dartFlag, *docFlag, *goFlag, *pyFlag, *tsFlag = true, true, true, true, true
 		fmt.Println("[codegen] No flag => generate code for all languages")
 	}
 
 	ref := core.GetRef()
 	ref = core.Sanitize(ref)
 
-	if enableAll || *dartf {
+	if *dartFlag {
 		fmt.Println("[codegen] Generating Dart code")
 		dart.GenCode(ref)
 	}
 
-	if enableAll || *docf {
+	if *docFlag {
 		fmt.Println("[codegen] Generating documentation")
 		doc.GenDoc(ref)
 	}
 
-	if enableAll || *gof {
+	if *goFlag {
 		fmt.Println("[codegen] Generating Go code")
 		golang.GenGo(ref)
 	}
 
-	if enableAll || *pyf {
+	if *pyFlag {
 		fmt.Println("[codegen] Generating Python code")
 		py.GenPy(ref)
 	}
 
-	if enableAll || *tsf {
+	if *tsFlag {
 		fmt.Println("[codegen] Generating Typescript code")
 		ts.GenTs(ref)
 	}
